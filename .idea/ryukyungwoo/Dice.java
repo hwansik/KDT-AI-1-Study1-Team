db898e78a4cdc8935b9b1925c3c50e586e1e9687
ryukyungwoo <ryukyungwoo1220@gmail.com>
2023-04-02 01:45:00 +0900

    feat: [유경우] 주사위 만들기 도중 [DICEx]

---
+++ .idea/ryukyungwoo/Dice.java
@@ -0,0 +1,18 @@
+package practice.dice3;
+
+public class Dice {
+    final private int MAX_NUMBER = 6;
+    final private int MIN_NUMBER = 1;
+    final private int diceNumber;
+    t) ((Math.random() * (MAX_NUMBER + MIN_NUMBER - 1 )) + MIN_NUMBER);
+}
+
+    @Override
+    public String toString() {
+        return "Dice{" +
+                "diceNumber=" + diceNumber +
+                '}';
+    }
+    public Dice () {
+        this.diceNumber = (in
+}
