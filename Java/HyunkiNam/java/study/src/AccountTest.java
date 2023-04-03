c370831c36b2a8dd479f015888406a0a4842299f
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-04-03 17:44:19 +0900

    update: [남현기] Main -> AccountTest 이름 변경 [STUDYx]

--- Java/HyunkiNam/java/study/src/AccountTest.java
+++ Java/HyunkiNam/java/study/src/AccountTest.java
@@ -1,12 +1,14 @@
 import problem.Account;
 
-public class Main {
+public class AccountTest {
     public static void main(String[] args) {
 
         Account account = new Account("홍길동", 300000);
 
         account.deposit(50000);
         account.withdraw(30000);
+
+        account.withdraw(5000000);
 
         System.out.println(account);
     }
